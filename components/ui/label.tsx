f4130b7b40686b2f87f7bef8bc2448c847f205e8
1998-vamsi <a.vamsikrishnayadav@gmail.com>
2024-06-18 12:09:33 +0530

    developing the too

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -9,7 +9,9 @@ import { cn } from "@/lib/utils"
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
-
+const labelVariants = cva(
+  "text-sm font-length"
+)
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
